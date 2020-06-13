8b7bbc1e7c67893bc16c0ac3284be800798673a4
Daniel B <daniel@ardreamcenter.tv>
2020-06-13 07:15:08 -0500

    Review

---
+++ Review/Review.js
@@ -0,0 +1,98 @@
+// console.log("HELLO!")
+
+
+// if(100){
+//     console.log("HIII!")
+// }
+
+
+// // let n = 10;
+
+// // if (n > 0) {
+// //     console.log('n is valid')
+// // }
+// // else if (n < 100) {
+// //     console.log('n is valid')
+// // }
+// // else {
+// //     console.log(' n is not valid')
+// // }
+
+// // let n = 10;
+
+// // if (n > 0) {
+// //     console.log('n is valid')
+// // }
+// // if (n < 100) {
+// //     console.log('n is valid')
+// // }
+// // else {
+// //     console.log('n is not valid')
+// // }
+
+// // for(let i = 0; i< 10; i++){
+// //     console.log(i)
+// // }
+
+
+// let matrix = [
+//     ['a','b','c'],
+//     ['d','e','f'],
+//     ['g','h','i'],
+// ]
+
+// for (let i= 0; i < matrix.length; i++) {
+//     let subArr = matrix [i];
+
+//     for (let j = 0; j < subArr.length; j++) {
+//         console.log(subArr[j]);
+//     }
+// }
+
+// const person = {name: 'timothy', age: 58}
+
+
+// let msg =`hi i am a ${1+1+1+2} string i hope???`
+
+
+// function printReceipt(product, qty, price){
+// return `${product}$(${price}) * ${qty} = $${qty * price}`
+
+// }
+// printReceipt('butter', 4, 3.50)
+
+
+// for...of
+
+
+
+let colors = ['red', 'teal', 'cyan', 'yellow']
+
+// for(let i = 0; i<colors.length; i++){
+//     console.log(colors[i])
+// }
+
+// for(let color of colors){
+//     console.log(color)
+// }
+
+for(let char of 'PURPLE RAIN') {
+    console.log(char)
+}
+
+
+
+
+const chicken = {
+    name: 'lady gray',
+    age: 4,
+    color: 'black'
+}
+
+// for(let prop in chicken){
+//     console.log(`${prop}->${chicken[prop]}`)
+// }
+
+for(let key in colors){
+    console.log(key)
+}
